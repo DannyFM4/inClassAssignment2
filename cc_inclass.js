c981e7da28a30a2d590c2d9f1cfc0530a05135e8
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-11 16:06:31 -0500

    Completed Task 1

---
+++ cc_inclass.js
@@ -0,0 +1,12 @@
+// Task 1
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Notebook", price: 5, category: "Stationery"}
+];
+
+// function getProductsByCategory(products, category) {
+//   return products.filter(product => product.category === category);  // => means such that
+// };
+
+// console.log(getProductsByCategory(products, "Electronics"));
